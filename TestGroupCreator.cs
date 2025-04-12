bd43d429510950a71591ac0b383f1093baef6ba7
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-12 17:35:32 -0400

    Removed funcProgramRegistryTag from funcGetFuncCatchCode

--- TestGroupCreator.cs
+++ TestGroupCreator.cs
@@ -193,7 +193,6 @@ namespace TestGroupCreator
             dCatchTable.Add("funcPrintParameterSyntax", "f3");
             dCatchTable.Add("funcParseCmdArguments", "f4");
             dCatchTable.Add("funcProgramExecution", "f5");
-            dCatchTable.Add("funcProgramRegistryTag", "f6");
             dCatchTable.Add("funcCreateDSSearcher", "f7");
             dCatchTable.Add("funcCreatePrincipalContext", "f8");
             dCatchTable.Add("funcCheckNameExclusion", "f9");
